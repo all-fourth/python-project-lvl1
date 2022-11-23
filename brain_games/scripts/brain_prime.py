0e775b767236c0a8f8caa3ecbf6094bfd6c46451
Petro <petro@Petros-MBP.Dlink>
2022-11-23 22:58:15 +0300

    add brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.games.prime
+import brain_games.logic
+
+
+def main():
+    brain_games.logic.logic(brain_games.games.prime)
+
+
+if __name__ == '__main__':
+    main()
